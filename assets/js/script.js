59525e9fea962844795012ba2e814b67dd50a6f0
HoneyBuzz94 <144499231+HoneyBuzz94@users.noreply.github.com>
2023-11-11 15:11:00 -0700

    Added in scorecard information and put it at the end of the question cycle. Added comments into JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// HTML elements
 var startBtn = $(".start-btn");
 var questionCard = $(".question-card");
 var question = $(".question");
@@ -6,6 +7,11 @@ var option1 = $(".option1");
 var option2 = $(".option2");
 var option3 = $(".option3");
 var option4 = $(".option4");
+var scoreCard = $(".score-card");
+var initialBox = $(".initial-box");
+var submitBtn = $(".submit-btn");
+
+// JS global variables
 var questionArray = [
     q1 = {q: "What does 'var' stand for?", answers: ["Variable", "Variance", "Vertical Abstract Reporter", "Varsity"], correct: "Variable"},
     q2 = {q: "What command would you use to log to the console?", answers: ["console.log()", "print()", "logger.Log()", "writeToConsole()"], correct: "console.log()"},
@@ -14,19 +20,21 @@ var questionArray = [
 ];
 var questionCount = 0;
 
-init();
-
+// Initializing function
 function init(){
     questionCard.hide();
-    console.log(questionArray.length);
+    scoreCard.hide();
 }
+init();
 
+// This function initiates all other operations necessary to start the game
 function startGame(){
     startBtn.hide();
     fillQuestion();
     questionCard.show();
 }
 
+// This function updates the question card with each subsequent question's data
 function fillQuestion(){
     question.text(questionArray[questionCount].q);
     option1.text(questionArray[questionCount].answers[0]);
@@ -35,8 +43,8 @@ function fillQuestion(){
     option4.text(questionArray[questionCount].answers[3]);
 }
 
+// This function checks to see if the user answer was correct or wrong
 function userAnswer(event){
-    console.log(questionCount);
     if(event.target.textContent == questionArray[questionCount].correct){
         console.log("Correct!");
         questionCount++;
@@ -44,16 +52,33 @@ function userAnswer(event){
         console.log("Wrong!");
         questionCount++;
     }
+// When there are no more questions left, the function ends the game and displays the score card
     if(questionCount < questionArray.length){
         fillQuestion();
     } else {
-        console.log("Ya done");
+        showScore();
+        questionCount = 0;
     }
 }
 
+// This function displays the score card
+function showScore(){
+    questionCard.hide();
+    scoreCard.show();
+}
+
+// This function logs the player score and records their data in the high score sheet
+function logScore(){
+        scoreCard.hide();
+        startBtn.show();
+}
+
+// These are the event listeners for the page
 startBtn.on("click", startGame);
 
 options.on("click", ".option-btn", userAnswer);
+
+submitBtn.on("click", logScore);
 
 
 
