9d916a0878cef89da59ce5727a5cb5628f11bc98
HoneyBuzz94 <144499231+HoneyBuzz94@users.noreply.github.com>
2023-11-11 14:30:28 -0700

    Added to js. Hides question cards on start. Cycles through questions after clicking start. Tracks correct and incorrect responses.

---
+++ assets/js/script.js
@@ -0,0 +1,77 @@
+var startBtn = $(".start-btn");
+var questionCard = $(".question-card");
+var question = $(".question");
+var options = $(".options");
+var option1 = $(".option1");
+var option2 = $(".option2");
+var option3 = $(".option3");
+var option4 = $(".option4");
+var questionArray = [
+    q1 = {q: "What does 'var' stand for?", answers: ["Variable", "Variance", "Vertical Abstract Reporter", "Varsity"], correct: "Variable"},
+    q2 = {q: "What command would you use to log to the console?", answers: ["console.log()", "print()", "logger.Log()", "writeToConsole()"], correct: "console.log()"},
+    q3 = {q: "Which method could you use to hide an HTML element?", answers: ["style.display = 'none'", "style.display = hide", "setDisplay(hide)", "visibility = 'none'"], correct: "style.display = 'none'"},
+    q4 = {q: "Which of the following would you use to rotate an HTML element?", answers: [".transform", ".rotate", ".spin", ".adjust"], correct: ".transform"}
+];
+var questionCount = 0;
+
+init();
+
+function init(){
+    questionCard.hide();
+    console.log(questionArray.length);
+}
+
+function startGame(){
+    startBtn.hide();
+    fillQuestion();
+    questionCard.show();
+}
+
+function fillQuestion(){
+    question.text(questionArray[questionCount].q);
+    option1.text(questionArray[questionCount].answers[0]);
+    option2.text(questionArray[questionCount].answers[1]);
+    option3.text(questionArray[questionCount].answers[2]);
+    option4.text(questionArray[questionCount].answers[3]);
+}
+
+function userAnswer(event){
+    console.log(questionCount);
+    if(event.target.textContent == questionArray[questionCount].correct){
+        console.log("Correct!");
+        questionCount++;
+    } else {
+        console.log("Wrong!");
+        questionCount++;
+    }
+    if(questionCount < questionArray.length){
+        fillQuestion();
+    } else {
+        console.log("Ya done");
+    }
+}
+
+startBtn.on("click", startGame);
+
+options.on("click", ".option-btn", userAnswer);
+
+
+
+
+
+
+/* const shuffle = (array) => { 
+    for (let i = array.length - 1; i > 0; i--) { 
+      const j = Math.floor(Math.random() * (i + 1)); 
+      [array[i], array[j]] = [array[j], array[i]];
+    } 
+    return array; 
+};
+
+var shuffledArray = shuffle(questionArray);
+for(i=0;i<shuffledArray.length;i++){
+    for(j=0;j<shuffledArray[i].answers.length;j++){
+        shuffle(shuffledArray[i].answers[j]);
+    }
+}
+console.log(shuffledArray); */
